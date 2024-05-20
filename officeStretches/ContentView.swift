9866cccedcea1ac1b1a4914ad7836f7ebed98e6c
alexmaxu <12alexmaxu@gmail.com>
2024-05-20 08:09:51 +0200

    Initial Commit

---
+++ officeStretches/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  officeStretches
+//
+//  Created by Alex  on 20/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ officeStretches/officeStretchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  officeStretchesApp.swift
+//  officeStretches
+//
+//  Created by Alex  on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct officeStretchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
